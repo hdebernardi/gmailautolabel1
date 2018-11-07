b7e232998ebab01b8576f6d8b2508cb3b012ae6c
Toniniyo <44023587+Toniniyo@users.noreply.github.com>
2018-11-07 18:12:18 +0100

    Add files via upload

---
+++ mailautolabel/gestionCsv/functions.py
@@ -0,0 +1,18 @@
+import os
+
+#retourne 1 si le fichier "username.csv" est présent, 0 si absent
+def isPresent(username):
+    root=os.getcwd() 
+    #donne data/username.csv ou data\username.csv si window ou linux
+    rel_path = os.path.join("data",username+".csv")
+    #chemin correcte du fichier
+    abs_path = os.path.join(root, rel_path)
+
+    #on test si le fichier est présent en tentant de l'ouvrir
+    try:
+        with open(abs_path): pass
+    except IOError:
+        return 0 #fichier absent
+
+    return 1 #fichier présent
+
