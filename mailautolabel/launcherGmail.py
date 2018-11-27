df662118049e193756174fc6019399aaf0ac7ab7
Toniniyo <44023587+Toniniyo@users.noreply.github.com>
2018-11-27 04:08:23 +0100

    Update launcherGmail.py

--- mailautolabel/launcherGmail.py
+++ mailautolabel/launcherGmail.py
@@ -4,7 +4,7 @@ from httplib2 import Http
 from oauth2client import file, client, tools
 import email
 from apiclient import errors
-
+import ml.supervised
 
 from gmail.RecuperationMail import *
 import csv_helper
@@ -42,13 +42,22 @@ def AjoutLabel(service,labelId,messageId):
 
 
 #######################################################################
+#                                        #
+#######################################################################
+def creer_csv(username,service):
+        #on crée le fichier csv pour les mails déjà labélisés
+        final_list = AllMessage(service)
+        csv_helper.save_mails(username, final_list)
+        #on crée le fichier csv pour les mails non labélisés
+        final_list = MessagesNonLabelises(service)
+        csv_helper.save_mails("NON_LABEL"+username, final_list)
+
+
+#######################################################################
 #  			 Fonction principale                          #
 #######################################################################
-
 def connectGmail(username):
-    """Shows basic usage of the Gmail API.
-    Lists the user's Gmail labels.
-    """
+    #On se connecte au service de gmail
     store = file.Storage('token.json')
     creds = store.get()
     if not creds or creds.invalid:
@@ -56,11 +65,26 @@ def connectGmail(username):
         creds = tools.run_flow(flow, store)
     service = build('gmail', 'v1', http=creds.authorize(Http()))
    
-    # Ajoute le label UNREAD au premier mail
-    #sujet du message : Kevin, vous avez 46 nouvelles notification
-    #AjoutLabel(service = service,labelId = 'UNREAD',messageId = '167329c4039358c9' )
 
-    final_list = AllMessage(service=service)
-    csv_helper.save_mails(username, final_list)
+    #Si aucun fichier csv n'est crée pour username, c'est une première connection
+    if(csv_helper.is_present(username) == 0):
+        print("Première connection")
+        creer_csv(username,service)
+    #Sinon on propose deux choix à l'utilisateur
+    else:
+        choix=input("Désirez-vous:\n réentrainer votre IA (Taper 1)\n Labéliser vos mails (Taper 2)\n Saisir:")
+        #Réentrainer l'IA
+        if(choix == "1"):
+            creer_csv(username,service)
+        #Labéliser les mails
+        else:
+            print("--------------------------------------")
+            print("MACHINE LEARNING")
+            mails_nonlab = csv_helper.to_dict("NON_LABEL"+username)
+            prediction = ml.supervised.supervised_with_nolabelling_mail(username)
+            print("--------------------------------------")
+            print("Labélisation des mails en cours")
+            for i in range(len(prediction)):
+                AjoutLabel(service = service,labelId = prediction[i],messageId = mails_nonlab[i]['id'])
 
-
+    
