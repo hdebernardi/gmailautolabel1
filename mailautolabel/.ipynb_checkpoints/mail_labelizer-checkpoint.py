efcd03545053d27fbb3c50767402c8966166d4f8
Debernardi <contact@hippolyte-debernardi.com>
2018-12-02 04:52:24 +0100

    update

---
+++ mailautolabel/.ipynb_checkpoints/mail_labelizer-checkpoint.py
@@ -0,0 +1,19 @@
+import sys
+import csv_helper
+
+def main(argv):
+	username = 'm1.autolabel1@gmail.com'
+	#username = 'chucknorrism1luminy@gmail.com'
+
+	labelised = csv_helper.toDict('LABELISED_' + username)
+	unlabelised = csv_helper.toDict('UNLABELISED_' + username)
+
+    predictions = ml.supervised.predictLabel(labelised, unlabelised)
+
+	# on ouvre une connexion afin d'appliquer les labels
+	service = gmail.connection.open(verbose)
+	for prediction in predictions :
+		gmail.mail.ajoutLabel(service, prediction['id'], prediction['label'])
+	
+if __name__ == '__main__':
+    main(sys.argv)
