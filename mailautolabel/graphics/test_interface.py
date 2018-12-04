d8c4dc16778a085f7e481fea2166f6fb62ada0f1
Toniniyo <44023587+Toniniyo@users.noreply.github.com>
2018-12-05 00:55:26 +0100

    Delete test_interface.py

--- mailautolabel/graphics/test_interface.py
+++
@@ -1,7 +0,0 @@
-from interface_graphique import affichageTexteSimple
-
-def testAffichageTexteSimple():
-	affichageTexteSimple("L'affichage de texte fonctionne bien.\n Il suffit d'appeler la fonction avec Texte.\n Texte etant ce qu'on veux afficher.")
-#Test fonction d'affichage simple
-
-testAffichageTexteSimple()
