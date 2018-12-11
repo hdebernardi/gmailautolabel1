87180eb38cf5d55c178f4f28dd1c2e3f401f7a0a
AstouGueye <43244383+AstouGueye@users.noreply.github.com>
2018-12-11 16:55:22 +0100

    Update mail.py

--- mailautolabel/extraction/imap/mail.py
+++ mailautolabel/extraction/imap/mail.py
@@ -6,6 +6,12 @@ import bs4
 
 ################################################################################
 def decodeHtml(html):
+	'''
+	- Decode du text au format html en un text simple sans format.
+	- Paramètre :
+		* ``html`` : Un text au format html.
+	- Renvoie ``text``, le text html décodé. '''
+	
 	soup = bs4.BeautifulSoup(html, 'html.parser')
 	
 	# Détruit tous les éléments de script et de style
@@ -28,6 +34,12 @@ def decodeHtml(html):
 
 ################################################################################
 def getHeader(mail):
+	'''
+	- Récupère l'entète du mail.
+	- Paramètre :
+		* ``mail`` : Un mail.
+	- Retourne ``dict_to_return`` contenant l'entète du mail.'''
+	
 	dict_to_return = {}
 	
 	for key in mail.keys():
@@ -40,6 +52,12 @@ def getHeader(mail):
 
 ################################################################################
 def getFlags(mail):
+	'''
+	- Récupère les labels du mail.
+	- Paramètre :
+		* ``mail`` : un mail.
+	- Retourne le dictionnaire ``dict_to_return`` contenant la liste des labels. '''
+	
 	dict_to_return = {'Flags': []}
 
 	for flag in mail:
@@ -49,6 +67,12 @@ def getFlags(mail):
 
 ################################################################################
 def getBody(mail):
+	'''
+	- Récupère le corps du mail.
+	- Paramètre :
+		* ``mail`` : Un mail.
+	- Retourne le dictionnaire ``dict_to_return`` contenant le corps du message.'''
+	
 	dict_to_return = {'Content-type': []}
 
 	has_a_text_part = 0
@@ -77,6 +101,13 @@ def getBody(mail):
 
 ################################################################################
 def getFolders(connection, verbose=False):
+	'''
+	- Récupère les dossiers présents dans la boite mail.
+	- Paramètres :
+		* ``connection`` : une connection vers une boite mail.
+		* ``verbose`` : permet d'afficher ce que la fonction fait (default = False).
+	- Retourne ``folders``, la liste des dossiers.
+	'''
 	if(verbose):
 		print('Getting folders...')
 		
@@ -93,6 +124,13 @@ def getFolders(connection, verbose=False):
 
 ################################################################################
 def getMails(connection, verbose=False):
+	'''
+	- Récupère les mails présents dans une boite mail en utilisant les fonctions précédentes.
+	- Paramètres :
+		* ``connection`` : une connection vers une boite mail.
+		* ``verbose`` : permet d'afficher ce que la fonction fait (default = False).
+	- Retourne ``all_messages``, une liste de mails.
+	'''
 	all_messages=[]
 	
 	folders = getFolders(connection, verbose)
@@ -129,6 +167,12 @@ def getMails(connection, verbose=False):
 
 ################################################################################
 def getUsefulPartsOfMails(mails):
+	''' 
+	- Ne récupère seulment les parties utiles des mails.
+	- Paramètre :
+		* ``mails`` : une liste de mails.
+	- Retourne ``all_mails``, la liste des mails traitée.
+	'''
 	all_mails = []
 
 	data = [[k,v] for mail in mails for k, v in mail.items()] 
