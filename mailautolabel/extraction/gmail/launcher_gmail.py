e7c43394c8042a7adcb7d0d17f05b919ee89f5c9
Maxime Gabiot <43213726+MaximeGab@users.noreply.github.com>
2018-12-11 23:02:32 +0100

    Update launcher_gmail.py

--- mailautolabel/extraction/gmail/launcher_gmail.py
+++ mailautolabel/extraction/gmail/launcher_gmail.py
@@ -11,15 +11,15 @@ import csv_helper
 def extracteur(username,service,type_extraction,label=None,fenetre=None):
     """
     Créer un fichier csv pour les mails déjà labélisés et ceux non labélisés
-	- Paramètres :
-		- ``username`` : identifiant de l'utilisateur (addresse mail).
-		- ``service`` : la connection avec la boite Gmail.
-		- ``type_extraction`` : si == "NON_LABEL", renvoie les mails non labélisés, tout sinon.
-		- ``label`` : Zone de text de destination si en mode graphique, inexistant sinon.
+    - Paramètres :
+        - ``username`` : identifiant de l'utilisateur (addresse mail).
+        - ``service`` : la connection avec la boite Gmail.
+        - ``type_extraction`` : si == "NON_LABEL", renvoie les mails non labélisés, tout sinon.
+        - ``label`` : Zone de text de destination si en mode graphique, inexistant sinon.
         - ``fenetre`` : Fenetre de destination pour affichage si en mode graphique, inexistante sinon.
-	- Retourne un tuple :
-		- ``labelisedMails`` : la liste des mails labélisés.
-		- ``unlabelisedMails`` : la liste des mails non-labélisés.
+    - Retourne un tuple :
+        - ``labelisedMails`` : la liste des mails labélisés.
+        - ``unlabelisedMails`` : la liste des mails non-labélisés.
     """
     test = mail.recupAllMessages(service,type_extraction)
     allMails = mail.allMessage(service,type_extraction,label,fenetre)
